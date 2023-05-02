7a6a9506dd1f55814b1a8ae405e20a555230ce32
Muhammad Rejka Permana <permanarejka@gmail.com>
2023-05-02 12:18:18 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  resources :rooms do
+    collection do
+      get :personal_chat
+      get :group_chat
+    end
+    resources :messages
+  end
+  root 'pages#home'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'user/:id', to: 'users#show', as: 'user'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
