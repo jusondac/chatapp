3ea58199b030139b757fce785d86cb8b0d699929
rejka permana <permanarejka@gmail.com>
2023-11-06 03:10:50 +0700

    update viee login & signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   root 'pages#home'
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
+    # sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
