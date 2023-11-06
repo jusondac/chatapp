e4620c2f226149410c4a269ed692a293bcbcdad6
rejka permana <permanarejka@gmail.com>
2023-11-07 03:34:37 +0700

    update views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    helper_method :check_before
+
+    def check_before(message)
+        message.previous_message.present? && message.previous_message.user_id.eql?(message.user.id)
+    end
 end
